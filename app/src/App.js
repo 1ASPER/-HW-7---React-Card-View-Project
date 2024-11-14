ae4e1d79376202c83ab737e452f14328bc6f4a72
Nazar Bapanov <bapanov_n@au.kg>
2024-11-14 10:45:34 +0600

    added css

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,34 @@
 import { ProductCard } from "./components/ProductCard"; 
 
+const products = [
+  {
+    id: 0,
+    name: "Product 1",
+    price: 100,
+    category: "Category 1",
+    image: "https://pics.craiyon.com/2023-09-29/cc9c956c1a674b78a0c7e4f11cef1190.webp",
+  },
+
+  {
+    id: 1,
+    name: "Product 2",
+    price: 200,
+    category: "Category 2",
+    image: "https://static.displate.com/857x1200/displate/2021-08-28/660ac65185e723aede15a0c413a4ca83_a96df10c8960ab9365eb04fd13e18105.jpg",
+  },
+]
+
 function App() {
   return (
     <div>
-      <ProductCard />
+      <h1>Products</h1>
+      <div>
+
+        {products.map((product) => (
+          <ProductCard data={product} />
+        ))}
+
+      </div>
     </div>
   );
 }
--- app/src/components/ProductCard/index.jsx
+++ app/src/components/ProductCard/index.jsx
@@ -1,7 +1,15 @@
-export function ProductCard () {
+import "./index.css";
+
+export function ProductCard (props) {
+
+    const { image, category, name, price } = props.data;
+
     return (
-        <div>
-            Product Card
+        <div className="product-card">
+            <img src={image} alt="" />
+            <div>{category}</div>
+            <h4>{name}</h4>
+            <div>{price}</div>
         </div>
     );
 }
