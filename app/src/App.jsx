4181fb4046b72363bf6b96dec932e2d74cbc1a39
Nazar Bapanov <bapanov_n@au.kg>
2024-11-19 10:50:03 +0600

    added request to API & Hooks

---
+++ app/src/App.jsx
@@ -0,0 +1,32 @@
+import { ProductCard } from "./components/ProductCard";
+import { useState, useEffect } from "react"; 
+import "./index.css";
+
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Calls some function when the component is updated by State change
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/')
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+            .finally(() => setLoading(false));  
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => setProducts([])}>button</button>
+      <div className="product-list">
+        {loading && <div>Loading...</div>}
+        {products.map((product) => (
+          <ProductCard data={product} />
+        ))}
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- app/src/components/ProductCard/index.jsx
+++ app/src/components/ProductCard/index.jsx
@@ -2,14 +2,14 @@ import "./index.css";
 
 export function ProductCard (props) {
 
-    const { image, category, name, price } = props.data;
+    const { image, category, title, price } = props.data;
 
     return (
         <div className="product-card">
             <img src={image} alt="" />
             <div>{category}</div>
-            <h4>{name}</h4>
-            <div>{price}</div>
+            <h4>{title}</h4>
+            <div>{price}$</div>
         </div>
     );
 }
